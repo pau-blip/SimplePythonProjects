4a86bc4dc4037f087d0f8216e1b27d20ffc7b092
pau-blip <63321701+pau-blip@users.noreply.github.com>
2021-08-10 17:51:55 -0400

    Add files via upload

---
+++ Mad Libs Game.py
@@ -0,0 +1,18 @@
+
+#Mad Libs Game
+Animal_name = input("pick a zoo animal :")
+adjective = input("Pick an adjective: ")
+verb = input("Pick a verb: ")
+
+print('Today I went to the zoo. I saw a', Animal_name)
+print("The",Animal_name)
+print("was",adjective)
+print("The",Animal_name)
+print("was", verb)
+
+
+
+
+
+
+
