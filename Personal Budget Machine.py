ab9eca5546e9726c5d99036803ec22352293cb83
pau-blip <63321701+pau-blip@users.noreply.github.com>
2022-01-02 17:15:51 -0500

    Add files via upload

---
+++ Personal Budget Machine.py
@@ -0,0 +1,12 @@
+
+
+print("Welcome to the personal budget machine")
+name = input("What is your name")
+print("hello",name)
+
+Monthly_income = float(input("What is your monthly income"))
+Monthly_expense = float(input("What is your monthly expense"))
+
+saved_money = (Monthly_income - Monthly_expense)
+
+print("hello",name,"You Will save", saved_money,"This Month!")
